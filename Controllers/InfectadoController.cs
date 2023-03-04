663a37c35124936af62a14443f096e0c7e0d3619
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-04 19:55:38 -0300

    Verbo HttpPut ok

--- Controllers/InfectadoController.cs
+++ Controllers/InfectadoController.cs
@@ -35,5 +35,13 @@ namespace Api.Controllers
             
             return Ok(infectados);
         }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+            
+            return Ok("Atualizado com sucesso");
+        }
     }
 }
